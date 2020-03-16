2fe62c53b3c6db072c4c20f40649709e6c5fca2e
ante96 <aantenucci1@gmail.com>
2020-03-16 20:22:16 +0100

    aggiunta schermata di gioco

---
+++ examples/chat/public/table.js
@@ -0,0 +1,36 @@
+function Card(card)
+{
+    if (card) {
+        this.suit = card.suit;
+        this.value = card.value;
+        this.id = card.id;
+        this.new_value = card.new_value;
+    }
+}
+
+Card.prototype.get_suit = function() {
+    return this.suit
+}
+
+function CardsGroup(type, id, owners)
+{
+    this.type = type;
+    this.id = id;
+    this.owners = owners;
+    this.covered = true;
+    this.side_cards = [];
+}
+CardsGroup.prototype = new Array;
+
+CardsGroup.prototype.toObject = function()
+{
+    return {
+        "id": this.id,
+        "type": this.type,
+        "owners": this.owners,
+        "covered": this.covered,
+        "length": this.length,
+        "side_cards": this.side_cards
+    }
+}
+
