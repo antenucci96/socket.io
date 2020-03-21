976aa9cfc27faee36d604b058e977a1445c1fdcf
ante96 <aantenucci1@gmail.com>
2020-03-21 16:53:10 +0100

    modifiche

--- examples/chat/public/main.js
+++ examples/chat/public/main.js
@@ -248,7 +248,7 @@ $(function () {
     socket.on('login', (data) => {
         connected = true;
         // Display the welcome message
-        var message = "Welcome to Socket.IO Chat – ";
+        var message = "Benvenuto nel giretto";
         log(message, {
             prepend: true
         });
@@ -372,16 +372,8 @@ var myGameArea = {
     start: function () {
         this.canvas.width = 800;
         this.canvas.height = 460;
-        //this.canvas.style.left = "460px";
         this.context = this.canvas.getContext("2d");
-        // var background = new Image();
-        // background.src = "tiles-2797094_1280.jpg";
-        // background.onload = function(){
-        //     ctx.drawImage(background,0,0);
-        // };
         document.querySelector('#play').appendChild(this.canvas);
-        // this.frameNo = 0;
-        // this.interval = setInterval(updateGameArea, 20);
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
